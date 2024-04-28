8cb56b8f3f816f3164e12a9da5ab9aeeb7af93cd
imashish22 <imjhaashish22@gmail.com>
2024-04-28 19:14:39 +0530

    updatte project

--- src/Resources/projects.js
+++ src/Resources/projects.js
@@ -3,7 +3,7 @@ export const projects = [
     technologies: [],
     title: "Portfolio",
     images:
-      "https://th.bing.com/th?id=OIP.aVXY6ONMOpJY9K5zCn9WYgHaEu&w=313&h=199&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2",
+      "https://th.bing.com/th/id/OIGP.bvu6srNVpSe6TkqBIbH5?w=251&h=251&c=6&o=5&dpr=1.3&pid=1.7",
     desription:
       " This is an Portfolio webisite made using HTML CSS and JAVASCRIPT . ",
     link: "/",
@@ -11,7 +11,7 @@ export const projects = [
   {
     technologies: [],
     title: "StudyAT",
-    images: "https://sheysathya2.netlify.app/pizzas.svg",
+    images: "https://assets.materialup.com/uploads/d88b3eb4-2cdd-4f4f-8768-d7f720b6aaf2/preview.png",
     desription:
       "StudyAT is an virtual study app  platform made using flutter,firebase and zegocloud. At studyat student can interact using live videocalls they can chat to each other. ",
     link: "/",
@@ -20,7 +20,7 @@ export const projects = [
   {
     technologies: [],
     title: "Weather App",
-    images: "https://sheysathya2.netlify.app/pizzas.svg",
+    images: "https://i.pinimg.com/originals/58/d3/e8/58d3e8c201301c2f9e7710e834c7025d.png",
     desription:
       " This is a Weather Application made with NodeJS. this Application will help you to search or navigate the current weather of your city or other city . It is easy to use.",
     link: "https://mausaum-dekho.onrender.com",
